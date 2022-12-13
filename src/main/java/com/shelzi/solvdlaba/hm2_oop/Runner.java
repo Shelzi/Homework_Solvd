7ad0c293d587be9b8baef572c5c486838ab64639
shelzi <nemant38@gmail.com>
2022-12-13 19:06:33 +0300

    impl of strings-files-util task

--- src/main/java/com/shelzi/solvdlaba/hm2_oop/Runner.java
+++ src/main/java/com/shelzi/solvdlaba/hm2_oop/Runner.java
@@ -13,11 +13,19 @@ import com.shelzi.solvdlaba.hm2_oop.banksystem.model.service.BankService;
 import com.shelzi.solvdlaba.hm2_oop.banksystem.model.service.impl.BankAccountServiceImpl;
 import com.shelzi.solvdlaba.hm2_oop.banksystem.model.service.impl.BankServiceImpl;
 
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.LogManager;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Runner {
     private static final BankService bankService = BankServiceImpl.getInstance();
@@ -26,7 +34,13 @@ public class Runner {
     private static final Logger logger = LogManager.getRootLogger();
 
     public static void main(String[] args) {
-        Bank a = bankGenerator.generate(1).stream().findAny().get();
+
+        try { Map<String, Integer> wordsCount = new HashMap<>();
+            for (String s : Arrays.stream(StringUtils.split(FileUtils.readFileToString(new File("src/main/resources/in.txt"), StandardCharsets.UTF_8.name()), " |\\,|\\.|\\!|\\?|\\(|\\)|\\[|\\]|\\;|\\:|\\|\\\n|\\\r|\\\t")).toList()) {wordsCount.put(s, wordsCount.getOrDefault(s, 0) + 1);} // //get input from file and counting
+            for (Map.Entry<String, Integer> e : wordsCount.entrySet()) {FileUtils.writeStringToFile(new File("src/main/resources/out.txt"), e.getKey() + " : " + e.getValue() + "\n", StandardCharsets.UTF_8.name(), true);} // write result to output file
+        } catch (IOException e){ throw new RuntimeException(e);}
+
+        /*Bank a = bankGenerator.generate(1).stream().findAny().get();
         Bank b = bankGenerator.generate(1).stream().findAny().get();
         Bank c = bankGenerator.generate(1).stream().findAny().get();
         //test
@@ -65,6 +79,6 @@ public class Runner {
             logger.log(Level.INFO, customer.getBankAccounts());
         } catch (ServiceException e) {
             logger.log(Level.WARN, e);
-        }
+        }*/
     }
 }
